bd0ed60b7ae8bac8250ff349f52890935430802f
Adam Weingram <adamweingram@gmail.com>
2020-12-06 08:12:05 -0800

    Implemented insertion of simulation events

--- index.js
+++ index.js
@@ -73,10 +73,21 @@ async function insertSimRun(uid, dashbaordLayout, callback) {
     });
 }
 
+// Helper function to insert a new simulation event row
+async function insertSimEvent(uid, runID, timeStamp, name, category, intendedTarget, tags, payload) {
+    // Turn tags into a comma-separated string
+    const tagsString = tags.toString();
+
+    // Insert into the DB
+    db.run("INSERT INTO simulation_events (uid, runID, timeStamp, name, category, intendedTarget, tags, payload) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [
+        uid, runID, timeStamp, name, category, intendedTarget, tagsString, payload
+    ], handleSQLError);
+}
+
 app.get("/", (req, res) => {
     res.send({
         "status": "success",
-        "data": "Hello world!"
+        "data": "This server does not provide functionality here."
     });
 });
 
@@ -96,21 +107,43 @@ app.post("/", async (req, res) => {
     else if (req.body.type == "insertSimRun") {
         // Outline required elements
         const associatedUID = req.body.uid;
-        const dashboardLayout = req.body.dashbaordLayout;
+        const dashboardLayout = req.body.dashboardLayout;
         const instructions = req.body.instructionList;
         const simulationEvents = req.body.simulationEvents;
 
         // Attempt to insert a sim run into the DB
         await insertSimRun(associatedUID, dashboardLayout, (runID) => {
+
+            // Insert all the instructions into the DB
+            for (const i in instructions) {
+                // Organize required parts
+                const instruction = instructions[i].instruction;
+                const payload = instructions[i].payload;
+
+                // [TODO] Call create instruction row function
+            }
+
+            // Insert all the simulation events into the DB
+            for (const i in simulationEvents) {
+                // Organize required parts
+                const timeStamp = simulationEvents[i].timestamp;
+                const name = simulationEvents[i].name;
+                const category = simulationEvents[i].category;
+                const intendedTarget = simulationEvents[i].intendedTarget;
+                const tags = simulationEvents[i].tags;
+                const payload = simulationEvents[i].payload;
+
+                // Call create simulation event function
+                insertSimEvent(associatedUID, runID, timeStamp, name, category, intendedTarget, tags, payload);
+            }
+
+
+            // Send successful response
             res.send({
                 "status": "success",
                 "runID": runID
             });
         });
-
-        // [TODO] Insert all the instructions into the DB
-
-        // [TODO] Insert all the simulation events into the DB
     }
 
 });
